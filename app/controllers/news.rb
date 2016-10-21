24ef1b04f8d67872f820f5ebf8e9e955e13a10fe
Greg Garner & Henry Fyfe <pair+garner412.hfyfe@devbootcamp.com>
2016-10-21 10:59:31 -0500

    reformat sources and articles pages to have layout

--- app/controllers/news.rb
+++ app/controllers/news.rb
@@ -1,12 +1,16 @@
 get '/news' do
   resp = Newsapi.get_sources
   @sources = resp["sources"]
+  pp @sources
   erb :'news/index'
 end
 
 get '/news/:sourcename' do
   @resp = Newsapi.search(params[:sourcename])
-  pp @resp
-  erb :'news/show'
+  if @resp["status"] != "error"
+    erb :'news/show'
+  else
+    redirect "/news"
+  end
 end
 
